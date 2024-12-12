5e1871d096bb049bb412998640a4314160a83dd3
이강산 <bybark4785@gmail.com>
2024-12-12 11:40:34 +0900

    Feat : Exception 및 Error 작성

---
+++ src/main/java/com/elice/sdz/global/exception/order/InvalidOrderException.java
@@ -0,0 +1,9 @@
+package com.elice.sdz.global.exception.order;
+
+public class InvalidOrderException extends RuntimeException {
+
+    public InvalidOrderException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/elice/sdz/global/exception/user/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.elice.sdz.global.exception.user;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
