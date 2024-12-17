dcee2fbbfabcab1b5fc2f4fd50d3dbddcf04c412
mayy24th <86235986+mayy24th@users.noreply.github.com>
2024-12-17 11:11:33 +0900

    feat : BaseEntity 추가에 따른 장바구니 코드 수정

--- src/main/java/com/elice/sdz/orderItem/entity/OrderItem.java
+++ src/main/java/com/elice/sdz/orderItem/entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.elice.sdz.orderItem.entity;
 
+import com.elice.sdz.global.entity.BaseEntity;
 import com.elice.sdz.user.entity.Users;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -17,14 +18,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "order_item")
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,7 +36,4 @@ public class OrderItem {
 
     @OneToMany(mappedBy = "orderItem", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItemDetail> orderItemDetails = new ArrayList<>(); // 장바구니 상품 세부 정보
-
-    @Column(name = "reg_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private Timestamp regDate;
 }
