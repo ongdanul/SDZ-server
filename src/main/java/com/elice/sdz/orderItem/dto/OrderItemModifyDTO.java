3abfe44e80b2b4523094648f0bb4f68738a3f682
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-01-02 14:49:01 +0900

    fix : 장바구니 DTO @ToString 추가

--- src/main/java/com/elice/sdz/orderItem/dto/OrderItemModifyDTO.java
+++ src/main/java/com/elice/sdz/orderItem/dto/OrderItemModifyDTO.java
@@ -2,9 +2,11 @@ package com.elice.sdz.orderItem.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class OrderItemModifyDTO {
     private Long productId;
     private int quantity;
