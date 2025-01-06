c49fe084023f77e84e6d14fd3960263da34bd4b3
jungjung <dmswn3649@naver.com>
2025-01-06 12:16:27 +0900

    fix:order

--- src/main/java/com/elice/sdz/order/repository/OrderRepository.java
+++ src/main/java/com/elice/sdz/order/repository/OrderRepository.java
@@ -1,8 +1,6 @@
 package com.elice.sdz.order.repository;
 
 import com.elice.sdz.order.entity.Order;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +9,5 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     //특정 사용자의 모든 주문을 리스트로 반환
-    Page<Order> findByUserEmail(String userEmail, Pageable pageable);
-
+    List<Order> findByUserEmail(String email);
 }
